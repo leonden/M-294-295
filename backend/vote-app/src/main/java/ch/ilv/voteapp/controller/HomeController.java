a92914a727ee4636578e87c7ab9752c9efc78abc
Leon <e033481@edu.sbl.ch>
2023-04-21 14:24:02 +0200

    controller

---
+++ backend/vote-app/src/main/java/ch/ilv/voteapp/controller/HomeController.java
@@ -0,0 +1,12 @@
+package ch.ilv.voteapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui.html";
+    }
+}
---
+++ backend/vote-app/src/main/java/ch/ilv/voteapp/controller/TestController.java
@@ -0,0 +1,14 @@
+package ch.ilv.voteapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String test() {
+        return "Hello";
+    }
+
+}
