c1ff7768f454675dc69ad6f62968cccb28f2ad9e
Leon <e033481@edu.sbl.ch>
2023-04-27 09:48:11 +0200

    user integration finished

--- backend/vote-app/src/main/java/ch/ilv/voteapp/controller/PersonController.java
+++ backend/vote-app/src/main/java/ch/ilv/voteapp/controller/PersonController.java
@@ -1,48 +1,66 @@
 package ch.ilv.voteapp.controller;
 
+import ch.ilv.voteapp.base.MessageResponse;
 import ch.ilv.voteapp.entity.Person;
-import ch.ilv.voteapp.repository.PersonRepository;
 import ch.ilv.voteapp.security.Roles;
+import ch.ilv.voteapp.service.PersonService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SecurityRequirement(name="bearerAuth")
 @RestController
+@Validated
 public class PersonController {
 
-    public final PersonRepository personRepository;
+    public final PersonService personService;
 
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
+    @GetMapping("api/v1/person")
+    @RolesAllowed(Roles.Read)
+    public ResponseEntity<List<Person>> all() {
+        List<Person> result = personService.getPersons();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
-    @RolesAllowed("admin")
+    @GetMapping("api/v1/person/{id}")
+    @RolesAllowed(Roles.Read)
+    public ResponseEntity<Person> one(@PathVariable Long id) {
+        Person person = personService.getPerson(id);
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
     @PostMapping("api/v1/person")
-    public String savePerson(@RequestBody Person person) {
-        personRepository.save(person);
-        return "\"" + person.getFirstName() + " " + person.getLastName() + "\" saved";
+    @RolesAllowed(Roles.Update)
+    public ResponseEntity<Person> newPerson(@Valid @RequestBody Person person) {
+        Person savedPerson = personService.insertPerson(person);
+        return new ResponseEntity<>(savedPerson, HttpStatus.OK);
     }
 
-    @RolesAllowed("admin")
-    @GetMapping ("api/v1/person")
-    public String getPerson(@RequestBody Person person) {
-        personRepository.save(person);
-        return "Got \"" + person.getFirstName() + " " + person.getLastName() + "\"";
+    @PutMapping("api/v1/person/{id}")
+    @RolesAllowed(Roles.Update)
+    public ResponseEntity<Person> updatePerson(@Valid @RequestBody Person person, @PathVariable Long id) {
+        Person savedPerson = personService.updatePerson(person, id);
+        return new ResponseEntity<>(savedPerson, HttpStatus.OK);
     }
 
-    @RolesAllowed("admin")
-    @DeleteMapping ("api/v1/person")
-    public String deletePerson(@RequestBody Person person) {
-        personRepository.save(person);
-        return "\"" + person.getFirstName() + " " + person.getLastName() + "\" deleted";
+    @DeleteMapping("api/v1/person/{id}")
+    @RolesAllowed(Roles.Admin)
+    public ResponseEntity<MessageResponse> deletePerson(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(personService.deletePerson(id));
+        } catch (Throwable t) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
-
 
 }
--- backend/vote-app/src/main/java/ch/ilv/voteapp/repository/PersonRepository.java
+++ backend/vote-app/src/main/java/ch/ilv/voteapp/repository/PersonRepository.java
@@ -3,5 +3,9 @@ package ch.ilv.voteapp.repository;
 import ch.ilv.voteapp.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByOrderByNameAscFirstnameAsc();
 }
