a83f991e47c0e90ad35a76e1c7b6a39b04ed9f48
Leon <e033481@edu.sbl.ch>
2023-05-26 16:09:47 +0200

    remove redirect

--- frontend/vote-app/src/app/app-routing.module.ts
+++ frontend/vote-app/src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
-import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
