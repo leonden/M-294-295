c12aeecc4932a4a21b7b261b4f16e5cab907cafe
Leon <e033481@edu.sbl.ch>
2023-05-26 16:10:04 +0200

    formatted

--- frontend/vote-app/src/app/services/app.auth.service.ts
+++ frontend/vote-app/src/app/services/app.auth.service.ts
@@ -1,19 +1,22 @@
-import {Injectable} from '@angular/core';
-import {JwtHelperService} from '@auth0/angular-jwt';
-import {AuthConfig, OAuthErrorEvent, OAuthService} from 'angular-oauth2-oidc';
-import {BehaviorSubject, Observable, of} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthConfig, OAuthErrorEvent, OAuthService } from 'angular-oauth2-oidc';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppAuthService {
   private jwtHelper: JwtHelperService = new JwtHelperService();
   private usernameSubject: BehaviorSubject<string> = new BehaviorSubject('');
-  public readonly usernameObservable: Observable<string> = this.usernameSubject.asObservable();
+  public readonly usernameObservable: Observable<string> =
+    this.usernameSubject.asObservable();
   private useraliasSubject: BehaviorSubject<string> = new BehaviorSubject('');
-  public readonly useraliasObservable: Observable<string> = this.useraliasSubject.asObservable();
+  public readonly useraliasObservable: Observable<string> =
+    this.useraliasSubject.asObservable();
   private accessTokenSubject: BehaviorSubject<string> = new BehaviorSubject('');
-  public readonly accessTokenObservable: Observable<string> = this.accessTokenSubject.asObservable();
+  public readonly accessTokenObservable: Observable<string> =
+    this.accessTokenSubject.asObservable();
 
   constructor(
     private oauthService: OAuthService,
@@ -37,8 +40,7 @@ export class AppAuthService {
   async initAuth(): Promise<any> {
     return new Promise<void>(() => {
       this.oauthService.configure(this.authConfig);
-      this.oauthService.events
-        .subscribe(e => this.handleEvents(e));
+      this.oauthService.events.subscribe((e) => this.handleEvents(e));
       this.oauthService.loadDiscoveryDocumentAndTryLogin();
       this.oauthService.setupAutomaticSilentRefresh();
     });
@@ -46,13 +48,25 @@ export class AppAuthService {
 
   public getRoles(): Observable<Array<string>> {
     if (this._decodedAccessToken !== null) {
-      return new Observable<Array<string>>(observer => {
-        if (this._decodedAccessToken.resource_access.demoapp.roles) {
-          if (Array.isArray(this._decodedAccessToken.resource_access.demoapp.roles)) {
-            const resultArr = this._decodedAccessToken.resource_access.demoapp.roles.map((r: string) => r.replace('ROLE_', ''));
+      return new Observable<Array<string>>((observer) => {
+        if (this._decodedAccessToken.resource_access.voteapp.roles) {
+          if (
+            Array.isArray(
+              this._decodedAccessToken.resource_access.voteapp.roles
+            )
+          ) {
+            const resultArr =
+              this._decodedAccessToken.resource_access.voteapp.roles.map(
+                (r: string) => r.replace('ROLE_', '')
+              );
             observer.next(resultArr);
           } else {
-            observer.next([this._decodedAccessToken.resource_access.demoapp.roles.replace('ROLE_', '')]);
+            observer.next([
+              this._decodedAccessToken.resource_access.voteapp.roles.replace(
+                'ROLE_',
+                ''
+              ),
+            ]);
           }
         }
       });
@@ -82,8 +96,14 @@ export class AppAuthService {
       this.accessTokenSubject.next(this._accessToken);
       this._decodedAccessToken = this.jwtHelper.decodeToken(this._accessToken);
 
-      if (this._decodedAccessToken?.family_name && this._decodedAccessToken?.given_name) {
-        const username = this._decodedAccessToken?.given_name + ' ' + this._decodedAccessToken?.family_name;
+      if (
+        this._decodedAccessToken?.family_name &&
+        this._decodedAccessToken?.given_name
+      ) {
+        const username =
+          this._decodedAccessToken?.given_name +
+          ' ' +
+          this._decodedAccessToken?.family_name;
         this.usernameSubject.next(username);
       }
 
