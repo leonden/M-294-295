e3e2f6fd7b9378b6db39423e371ca23271d1fa43
Leon <e033481@edu.sbl.ch>
2023-06-01 16:13:12 +0200

    election-card

--- frontend/vote-app/src/app/components/election-card/election-card.component.ts
+++ frontend/vote-app/src/app/components/election-card/election-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ElectionService } from 'src/app/services/election.service';
+import { Election } from 'src/app/dataaccess/election';
 
 @Component({
   selector: 'app-election-card',
   templateUrl: './election-card.component.html',
-  styleUrls: ['./election-card.component.scss']
+  styleUrls: ['./election-card.component.scss'],
 })
-export class ElectionCardComponent {
+export class ElectionCardComponent implements OnInit {
+  electionData: Election = {} as Election;
 
+  ngOnInit() {
+    const electionService = new ElectionService();
+    electionService
+      .getElections()
+      .subscribe((response) => (this.electionData = response));
+  }
 }
