086231191ff271d854032adf7d963abfe6234798
Leon <e033481@edu.sbl.ch>
2023-06-01 08:59:00 +0200

    remove unused code

--- frontend/vote-app/src/app/app.component.ts
+++ frontend/vote-app/src/app/app.component.ts
@@ -1,21 +1,8 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('toolbar') toolbar!: any;
-  @ViewChild('drawer') drawer!: any;
-
-  ngAfterViewInit() {
-    this.handleDrawerHeight();
-  }
-
-  handleDrawerHeight() {
-    const toolbarHeight = this.toolbar._elementRef.nativeElement.offsetHeight;
-    const drawer = this.drawer._elementRef.nativeElement;
-    drawer.style.height = `calc(100vh - ${toolbarHeight}px)`;
-  }
-}
+export class AppComponent {}
