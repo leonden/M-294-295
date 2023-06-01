8abc7e852e3cd04fab39d76755a4a927a6fccac7
Leon <e033481@edu.sbl.ch>
2023-06-01 16:10:25 +0200

    init utils.ts

---
+++ frontend/vote-app/src/app/utils.ts
@@ -0,0 +1,5 @@
+export const VOTE_BASE_URL = 'http://localhost:8081/api/v1/vote';
+export const ELECTION_BASE_URL = 'http://localhost:8081/api/v1/election';
+export const PERSON_BASE_URL = 'http://localhost:8081/api/v1/person';
+export const ELECTION_RESULT_BASE_URL =
+  'http://localhost:8081/api/v1/election-result';
