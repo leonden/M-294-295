dcb724ddc34ac063fe32131d87c5db96495bf190
Leon <e033481@edu.sbl.ch>
2023-06-01 08:58:37 +0200

    init elections page

---
+++ frontend/vote-app/src/app/components/pages/elections/elections.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elections',
+  templateUrl: './elections.component.html',
+  styleUrls: ['./elections.component.scss']
+})
+export class ElectionsComponent {
+
+}
