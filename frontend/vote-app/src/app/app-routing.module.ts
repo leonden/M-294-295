57d842bdd8932106130229871b0a4ea20c257223
Leon <e033481@edu.sbl.ch>
2023-06-02 11:35:42 +0200

    update routiing module

--- frontend/vote-app/src/app/app-routing.module.ts
+++ frontend/vote-app/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ElectionsComponent } from './components/pages/elections/elections.component';
+import { CreateElectionComponent } from './components/pages/create-election/create-election.component';
+import { AppRoles } from './app.roles';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'elections', component: ElectionsComponent },
+  {
+    path: 'create-election',
+    component: CreateElectionComponent,
+    data: { roles: [AppRoles.Admin] },
+  },
 ];
 
 @NgModule({
