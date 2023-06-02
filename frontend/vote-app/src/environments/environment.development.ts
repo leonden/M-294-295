44eee0eb92f382d72e06fc6090d019e9acf9c083
Leon <e033481@edu.sbl.ch>
2023-06-02 11:00:53 +0200

    minor adjustment

--- frontend/vote-app/src/environments/environment.development.ts
+++ frontend/vote-app/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   backendBaseUrl: 'http://localhost:9090/api/v1',
-  frontendBaseUrl: 'http://localhost-8umomf4:4200',
+  frontendBaseUrl: 'http://localhost:4200',
 };
